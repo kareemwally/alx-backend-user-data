2349ff05f1a7f772b16efb50e666e4cd072652b9
kareem <kareemwally@std.mans.edu.eg>
2024-11-08 01:32:51 +0200

    encryption

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,3 +10,8 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Validates that a given password matches the hashed password."""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
