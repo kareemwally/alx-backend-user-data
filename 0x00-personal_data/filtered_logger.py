fbe46b279a96b0e7a173af0d24c8eaacd30a740c
kareem <kareemwally@std.mans.edu.eg>
2024-11-08 01:00:16 +0200

    filtering

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,18 +1,36 @@
 #!/usr/bin/env python3
-"""
-simple module for logging system
-"""
+import logging
 import re
 from typing import List
 
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
-    """
-    filtering the message for logging file
-    """
     return re.sub(
         f"({'|'.join(fields)})=[^;]*",
         lambda m: f"{m.group(1)}={redaction}",
         message
     )
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s\
+              %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """
+        intiating a class instance
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        formatting the record
+        """
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.msg, self.SEPARATOR)
+        return super().format(record)
