fb961f8a28e8ac56cdc34e15c3729a537efe1c54
kareem <kareemwally@std.mans.edu.eg>
2024-11-13 00:07:15 +0200

    api

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -29,8 +29,8 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/api/v1/unauthorized/', methods=['GET'],\
-           strict_slashes=False)
+@app_views.route('/api/v1/unauthorized/', methods=['GET'],
+                 strict_slashes=False)
 def unauthorized() -> str:
     """ GET /api/v1/unauthorized
     Return:
