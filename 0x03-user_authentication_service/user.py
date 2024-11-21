0ba7c9b015fc87ba87452f0d6e8f0b920d976310
kareem <kareemwally@std.mans.edu.eg>
2024-11-21 19:01:52 +0200

    USER module

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+a simple module to define `User` model using SQLALchemy module
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+class User(Base):
+    """
+    the User model represnting the user table
+    """
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    session_id = Column(String, nullable=True)
+    reset_token = Column(String, nullable)
