a7f224933ea5b10eab27a1def5e56ae5b950afab
kareem <kareemwally@std.mans.edu.eg>
2024-11-22 02:05:01 +0200

    encrypt

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -9,6 +9,6 @@ def _hash_password(password):
     """
     using the bycrypt module to hash the password
     """
-    encoded = password.enode('utf-8')
+    encoded = password.encode('utf-8')
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(encoded, salt)
